3afd62e06136bbbee2d11a3dda3d58e3aea6d1d6
Richard Valentim de Souza <richardvalentim39@gmail.com>
2018-12-06 23:03:54 +0000

    Criação da classe Salao; Criação do namespace EXEC (para referenciar as funções 'execucao_barbeiro' e 'execucao_clientes');

--- src/mainThread.cpp
+++ src/mainThread.cpp
@@ -1,85 +1,113 @@
 #include <iostream>
-#include <pthread.h>
 #include <unistd.h>
 #include <semaphore.h>
-#define N_CADEIRAS 5
-#define N_CLIENTES 20
+#include <thread>
+#include "trabSO1.h"
 
-using namespace std;
-sem_t finaliza;    // segura a thread main até o barbeiro terminar
-sem_t clientes;
-sem_t mutex;	
+namespace EXEC{
+    unsigned int* buffer;			// armazena o numero do cliente
+    unsigned int atende_prox_cliente = 0;   // indica ao barbeiro qual o próximo cliente a ser atendido
+    unsigned int posicao_buffer = 0;    // indica a posição no buffer em que será inserido o cliente que acabou de chegar
 
-unsigned int* buffer;			// armazena o numero do cliente
-unsigned int atende_prox_cliente = 0;   // indica ao barbeiro qual o próximo cliente a ser atendido
-unsigned int posicao_buffer = 0;    // indica a posição no buffer em que será inserido o cliente que acabou de chegar
+    unsigned int esperando = 0;   
+    unsigned int i = 1;
+    unsigned int N_CADEIRAS;
+    sem_t clientes, mut;
 
-unsigned int esperando = 0;   
-unsigned int i = 1;
-         
-unsigned int cont = 0;   // (clientes o barbeiro atendeu) + (clientes que desistiram) 
-
-void* execucao_thread1_barbeiro(void* x){
-	while(cont < N_CLIENTES){
-		sem_wait(&mutex);
+    void execucao_barbeiro(Salao* S){
+        while(1){
+            sem_wait(&mut);
             if(esperando > 0){
-				esperando--;
-				sem_trywait(&clientes);   		 // somente decrementa o semáforo clientes, não adormece a thread caso clientes se torne zero
-				unsigned int cliente_atual = buffer[atende_prox_cliente++ % N_CADEIRAS];
-				printf("Atendendo o cliente %i...\n",cliente_atual);
-                cont++;
-				sem_post(&mutex);
-				sleep(2);     	 // tempo que o barbeiro leva pra atender;
-				printf("Terminei de atender o cliente %i\n",cliente_atual);
-			}
-			else{
-				sem_post(&mutex);
-				printf("Dormindo...\n");
-				sem_wait(&clientes);
-			}	
-	}
-    printf("Dormindo...\n");
-	sem_post(&finaliza);  
-	pthread_exit(NULL);
-}
-
-void* execucao_threadN_clientes(void* x){
-	sem_wait(&mutex); 
-		int id = i++;
-		printf("Cliente %i chegou...\n",id);
-
-		if(esperando < N_CADEIRAS){
-			esperando++;
-			sem_post(&clientes);
-			buffer[posicao_buffer++ % N_CADEIRAS] = id;
-		}
-		else{
-		    printf("Cliente %i desistiu, salao cheio...\n",id);
-            cont++;              
+                esperando--;
+                sem_trywait(&clientes);   		
+                unsigned int cliente_atual = buffer[atende_prox_cliente++ % N_CADEIRAS];
+                S->serving(cliente_atual);
+            }
+            else{
+                sem_post(&mut);
+                S->rest();
+                sem_wait(&clientes);
+            }	
         }
-	sem_post(&mutex);
-	pthread_exit(NULL);
+    }
+
+    void execucao_clientes(Salao* S){
+        sem_wait(&mut); 
+        int id = i++;
+        S->arrived(id);
+
+        if(esperando < N_CADEIRAS){
+            esperando++;
+            sem_post(&clientes);
+            buffer[posicao_buffer++ % N_CADEIRAS] = id;
+        }
+        else{
+            S->give_up(id);
+        }
+        sem_post(&mut);
+    }
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+using namespace EXEC;
+
+Salao::Salao(unsigned int Num_cadeiras){
+    N_CADEIRAS = Num_cadeiras;
+    buffer = new unsigned int[Num_cadeiras];
+           
+    sem_init(&clientes, 0, 0);
+	sem_init(&mut, 0, 1);
+}
+
+void Salao::inicializa_barbeiro(){
+        tB = std::thread(execucao_barbeiro, this);
+        //tB.join();       --> se habilitar isso, os clientes não executam. Não sei por que...
+}
+
+void Salao::inicializa_clientes(){
+    while(1){
+        tC = std::thread(execucao_clientes, this);
+        tC.join();
+        sleep(rand() % 4);         // intervalo de chegada dos clientes
+	}
+}
+
+void Salao::rest(){
+    printf("	Dormindo...\n");
+}
+
+void Salao::arrived(int id){
+    printf("Cliente %i chegou...\n",id);
+}
+
+void Salao::give_up(int id){
+    printf("Cliente %i desistiu, salao cheio...\n",id);
+}
+
+void Salao::serving(int cliente_atual){
+    printf("	Atendendo o cliente %i...\n",cliente_atual);
+    sem_post(&mut);
+    sleep(2);     	
+    printf("	Terminei de atender o cliente %i\n",cliente_atual);
+}
 
 int main(){
-	buffer = new unsigned int[N_CADEIRAS];
-
-	sem_init(&finaliza, 0, 0);  
-	sem_init(&clientes, 0, 0);
-	sem_init(&mutex, 0, 1);
+    Salao S(10);
 	
-	pthread_t barb;
-	pthread_t* cli = new pthread_t[N_CLIENTES];
-	
-	pthread_create(&barb, NULL, &execucao_thread1_barbeiro, NULL);    
-	
-	for(int j=0 ; j < N_CLIENTES ; j++){   
-		pthread_create(&cli[j], NULL, &execucao_threadN_clientes, NULL);   
-		sleep(rand() % 3);         // intervalo de chegada dos clientes
-	}
-	sleep(1);
-
-	sem_wait(&finaliza);   
+    S.inicializa_barbeiro();
+    S.inicializa_clientes();
+    sleep(10);
 }
+
 
